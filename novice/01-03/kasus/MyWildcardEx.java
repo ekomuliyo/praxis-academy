6f9e86119f466817fc865870fba7570f73948fac
Eko <ekomuliyo@gmail.com>
2019-10-27 19:40:10 +0700

    test push

--- novice/01-03/kasus/MyWildcardEx.java
+++ novice/01-03/kasus/MyWildcardEx.java
@@ -71,4 +71,8 @@ class CompBEmp extends Emp{
     public CompBEmp(String nm, int sal){
         super(nm, sal);
     }
+
+    public void testMethod (){
+        System.out.println("Hai");
+    }
 }
